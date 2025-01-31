ec371a705dc20f825b327de7403bd7764c03a052
mahdiyeh-abiyar <mahdiehabyari@gmail.com>
2025-01-31 15:07:41 +0330

    Add product card with Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  darkMode: 'class', // active dark mode with class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
